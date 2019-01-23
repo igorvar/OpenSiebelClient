7e7cf00170cf944992451bbd683522e685b2482a
igorvar <igorvar@users.noreply.github.com>
2019-01-23 09:50:47 +0200

    Add files via upload
    File need to be updated for your actual path

---
+++ RunSiebel_enu.js
@@ -0,0 +1,11 @@
+/*Target:
+C:\oracle\siebel\client\BIN\siebel.exe /c C:\oracle\siebel\client\bin\enu\sfs.cfg /b "C:\Program Files\Internet Explorer\iexplore.exe" /s C:\siebel_log\SqlLogEnu.log
+Strat in:
+C:\oracle\siebel\client\bin
+*/
+shell = new ActiveXObject("WScript.Shell");
+shell.Run("taskkill /F /IM iexplore.exe");
+shell.Run("C:\\oracle\\siebel\\client\\BIN\\siebel.exe /c C:\\oracle\\siebel\\client\\bin\\enu\\sfs.cfg /s C:\\siebel_log\\SqlLogEnu.log \b 'C:\\Program Files\\Internet Explorer\\iexplore.exe'");
+shell = null;
+
+
