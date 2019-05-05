abc15b22919dfb3e7869532f47eaedac8c83a096
igorvar <igorvar@users.noreply.github.com>
2019-05-05 10:06:13 +0300

    Update RunSiebel_enu.js

--- RunSiebel_enu.js
+++ RunSiebel_enu.js
@@ -5,7 +5,7 @@ C:\oracle\siebel\client\bin
 */
 shell = new ActiveXObject("WScript.Shell");
 shell.Run("taskkill /F /IM iexplore.exe");
-shell.Run("C:\\oracle\\siebel\\client\\BIN\\siebel.exe /c C:\\oracle\\siebel\\client\\bin\\enu\\sfs.cfg /s C:\\siebel_log\\SqlLogEnu.log \b 'C:\\Program Files\\Internet Explorer\\iexplore.exe'");
+shell.Run("C:\\oracle\\siebel\\client\\BIN\\siebel.exe /c C:\\oracle\\siebel\\client\\bin\\enu\\sfs.cfg /s C:\\siebel_log\\SqlLogEnu.log /b 'C:\\Program Files\\Internet Explorer\\iexplore.exe'");
 shell = null;
 
 
